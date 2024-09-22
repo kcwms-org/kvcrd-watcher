8da97a9909e790c6c47111120232fee91892b938
Kevin Williams <kevin@win11vm>
2024-09-21 22:38:05 -0700

    init: a filewatcher service

--- kvcdr-watcher-svc/Program.cs
+++ kvcdr-watcher-svc/Program.cs
@@ -1,7 +1,26 @@
+using NReco.Logging.File;
 using kvcdr_watcher;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddWindowsService(opt=>{
+            opt.ServiceName = "kvcdrWatcher";
+        });
 
-var host = builder.Build();
-host.Run();
+        LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+        
+        builder.Services.AddHostedService<Worker>();
+        builder.Services.AddLogging(lb =>
+        {
+            lb.AddFile(builder.Configuration.GetSection("Logging"));
+        });
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
--- kvcdr-watcher-svc/Worker.cs
+++ kvcdr-watcher-svc/Worker.cs
@@ -7,10 +7,14 @@ namespace kvcdr_watcher;
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IConfiguration _config;
+    private readonly FileSystemWatcher _watcher;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _config = configuration;
+        _watcher = new FileSystemWatcher();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -20,46 +24,49 @@ public class Worker : BackgroundService
             if (_logger.IsEnabled(LogLevel.Information))
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                var folderToWatch = Path.Combine(_config["FolderToWatch"]);
+
+                if (_watcher.EnableRaisingEvents == false || (!_watcher.Path.Equals(folderToWatch, StringComparison.OrdinalIgnoreCase) && Directory.Exists(folderToWatch)))
+                {
+                    _logger.LogInformation($"watching {folderToWatch}");
+
+                    _watcher.EnableRaisingEvents = false;
+                    _watcher.Created -= OnFileCreated;
+
+                    _watcher.Path = folderToWatch;
+                    _watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+                    _watcher.IncludeSubdirectories = false;
+                    _watcher.Created += OnFileCreated;
+
+                    _watcher.EnableRaisingEvents = true;
+                }
+
             }
             await Task.Delay(1000, stoppingToken);
         }
     }
 
-    public override Task StartAsync(CancellationToken cancellationToken)
-    {
-        if (cancellationToken.IsCancellationRequested)
-            return base.StartAsync(cancellationToken);
-
-        var folderToWatch = @"C:\Users\Quickemu\Desktop\Scans";
-        using (var watcher = new FileSystemWatcher(folderToWatch))
-        {
-            watcher.NotifyFilter = NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.LastWrite;
-            watcher.IncludeSubdirectories = false;
-
-            watcher.Created += OnFileCreated;
-        }
-
-        return Task.CompletedTask;
-    }
-
     private void OnFileCreated(object sender, FileSystemEventArgs e)
     {
-        if (File.Exists(e.FullPath))
+        _logger.LogInformation($"New file dropped: '{e.FullPath}'");
+
+        var sourceFolder = Path.GetDirectoryName(e.FullPath);
+        var archiveFolder = Path.Combine(sourceFolder, DateTime.Now.ToString("yyyy-MM-dd"));
+        var archivedFileName = Path.Combine(archiveFolder, Path.GetFileName(e.FullPath));
+        if (!Directory.Exists(archiveFolder))
         {
-            var process = new ProcessStartInfo("thunderbird.exe", $"--compose \"attachment='{e.FullPath}'\"");
+            Directory.CreateDirectory(archiveFolder);
         }
-        var destFolder = Path.GetDirectoryName(e.FullPath) ?? Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        var destPath = Path.Combine(destFolder, DateTime.Now.ToString("u"), Path.GetFileName(e.FullPath));
-        if (!Directory.Exists(destFolder))
-        {
-            Directory.CreateDirectory(destFolder);
-        }
-        File.Move(e.FullPath, destPath, true);
+        File.Move(e.FullPath, archivedFileName, true);
 
         var configuration = new Dictionary<string, string>();
-        configuration.Add("DROPPED_FILE_FULL_PATH", destPath);
+        configuration.Add("DROPPED_FILE_FULL_PATH", archivedFileName);
+        var id = Guid.NewGuid().ToString();
 
-        new EmailAttacher().Start(Guid.NewGuid().ToString(), configuration);
+        _logger.LogInformation($"Started EmailAttacher with ID='{id}' and DROPPED_FILE_FULL_PATH='{archiveFolder}'.");
+        new EmailAttacher().Start(id, configuration);
+        _logger.LogInformation($"Ended EmailAttacher with ID='{id}'");
 
     }
 }
